7984bcb1326c0fa086338064cc01f1259851d305
veryacademy <35196453+veryacademy@users.noreply.github.com>
2020-09-03 01:41:25 +0100

    nc

--- core/settings.py
+++ core/settings.py
@@ -138,13 +138,3 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        }
-    }
-}
